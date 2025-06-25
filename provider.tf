1daaba2b2e7aa5efd1b9c465579a245eff216cb9
wonshee <shee2.won@gmail.com>
2025-06-25 14:02:25 +0900

    test

--- provider.tf
+++ provider.tf
@@ -5,11 +5,12 @@ terraform {
       version = ">= 5.0.0"
     }
   }
+
   backend "s3" {
     bucket         = "terraform-20250625032048326100000001"
     key            = "terraform/state-test/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = ""
+    dynamodb_table = "dbdbdeep"
   }
 }
 
