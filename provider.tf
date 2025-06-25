74875555f0baab22dc69a4bc8294fba5b4b0a04b
wonshee <shee2.won@gmail.com>
2025-06-25 15:32:19 +0900

    terraform cloud test

--- provider.tf
+++ provider.tf
@@ -10,7 +10,6 @@ terraform {
     bucket         = "terraform-20250625032048326100000001"
     key            = "terraform/state-test/terraform.tfstate"
     region         = "ap-northeast-2"
-    dynamodb_table = "dbdbdeep"
   }
 }
 
